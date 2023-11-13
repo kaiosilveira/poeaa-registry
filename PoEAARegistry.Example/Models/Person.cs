cee47223a035a9b6bbdc3cda47a77fc2a7a88d5b
Kaio Silveira <silveira.kaio@icloud.com>
2023-11-13 20:10:02 +0100

    inplement `Person` class

---
+++ PoEAARegistry.Example/Models/Person.cs
@@ -0,0 +1,13 @@
+namespace PoEAARegistry.Example.Models;
+
+public class Person
+{
+  public readonly string FirstName;
+  public readonly string LastName;
+
+  public Person(string firstName, string lastName)
+  {
+    FirstName = firstName;
+    LastName = lastName;
+  }
+}
